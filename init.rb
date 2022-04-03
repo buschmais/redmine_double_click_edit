6e41549240692eab8c74573105357d9d3c3f8880
Frank Schwarz <frank.schwarz@buschmais.com>
2022-04-03 18:54:30 +0200

    Redmine 5 support

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 require 'redmine'
 
-require_dependency 'redmine_double_click_edit/hook'
+require_relative './lib/redmine_double_click_edit/hook'
 
 Redmine::Plugin.register :redmine_double_click_edit do
   name 'Double click issue description edit'
   author 'Frank Schwarz'
   description 'Double click to open the the issue description editor'
-  version '0.0.1.dev'
+  version '0.0.2.dev'
 end
--- lib/redmine_double_click_edit/hook.rb
+++ lib/redmine_double_click_edit/hook.rb
@@ -1,5 +1,5 @@
 module RedmineDoubleClickEdit
-    class Hooks < Redmine::Hook::ViewListener
+    class Hook < Redmine::Hook::ViewListener
       render_on :view_issues_form_details_bottom,
                 :partial => 'redmine_double_click_edit'
     end
